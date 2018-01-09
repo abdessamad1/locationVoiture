4b6ec46ef7e3cb6087dcbdcdafea5138c4acc45f
abdessamad idelkaid <abdessamadidel8@gmail.com>
2018-01-09 14:17:34 +0000

    init project

---
+++ index.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Abdou Kaid</title>
+	<!-- very nice reset -->
+	<link rel="stylesheet" href="css/normalize.css">
+	<!-- fonts awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- google fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
+	<!-- file css style -->
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/media.css">
+	<!-- Google Web Font -->
+	<link rel="stylesheet" href="http://fonts.googleapis.com/earlyaccess/droidarabickufi.css">
+</head>
+<body>
+
+<!-- Start Header -->
+<div class="header">
+	<div class="overlay">
+		<div class="container table">
+
+			<div class="navbar">
+				<span class="ts">Abdou<span class="main-color">Kaid</span></span>
+				<span class="links">
+					<ul>
+						<li class="hom ac"><i class="fa fa-home fa-lg" aria-hidden="true"></i> Home</li>
+						<li class="sub">Subscribe</li>
+						<li class="wrk">Our Work</li>
+						<li class="tm">Out team</li>
+						<li class="tst">Testimonial</li>
+						<li class="cont">contact</li>
+					</ul>
+				</span>
+			</div>
+
+			<div class="table-row">
+				<div class="intro text-center animation-element slide-left">
+					<h1 class="upper">We Design <span class="main-color">Things</span></h1>
+					<p>software designer writes in order to give a software development team <br> overall guidance to the architecture of the software project.</p>
+					<div class="buttons">
+						<button class="upper">Hire Us</button>
+						<button class="upper">Our Works</button>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<span class="arrow">
+				<i class="fa fa-chevron-circle-down fa-2x" aria-hidden="true"></i>
+		</span>
+	</div>
+</div>
+<!-- End Header -->
+
+
+<!-- Start Features -->
+<div class="features text-center">
+	<div class="container">
+
+		<div class="box animation-element slide-left">
+			<i class="fa fa-fax fa-3x"></i>
+			<h3>Print Design</h3>
+			<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+		</div>
+		<div class="box animation-element slide-left">
+			<i class="fa fa-desktop fa-3x" aria-hidden="true"></i>
+			<h3>Web Design</h3>
+			<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+		</div>
+		<div class="box animation-element slide-left">
+			<i class="fa fa-mobile fa-3x"></i>
+			<h3>App Design</h3>
+			<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+		</div>
+		<div class="box animation-element slide-left">
+			<i class="fa fa-magic fa-3x"></i>
+			<h3>Logo Design</h3>
+			<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+		</div>
+
+	</div>
+</div>
+<!-- End Features -->
+
+<!-- Start Subscribe -->
+<div class="subscribe">
+	<div class="overlay">
+		<div class="container">
+			<form>
+				<input type="email" name="mail" placeholder="Entrez your email adress for newsletter">
+				<button class="upper">Subscribe</button>
+			</form>
+		</div>
+	</div>
+</div>
+<!-- End Subscribe -->
+
+<!-- Start Oui Work -->
+<div class="our-work text-center">
+	<div class="container">
+		<h2 class="upper animation-element slide-left">our <span class="main-color">work</span></h2>
+		<p class="animation-element slide-left">software designer writes in order to give a software development team <br> overall guidance to the architecture of the software project</p>
+
+		<div class="items-box">
+
+			<div class="box">
+				<img src="img/img-storie/1-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="img/img-storie/35-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="img/img-storie/3-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="img/img-storie/4-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="img/img-storie/5-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="img/img-storie/6-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+			<div class="box hidden">
+				<img src="img/img-storie/87-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+			<div class="box hidden">
+				<img src="img/img-storie/89-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+			<div class="box hidden">
+				<img src="img/img-storie/25-400.jpeg" alt="test">
+				<div class="over text-center">
+					<h3 class="upper">Abazoo Website</h3>
+					<p>software designer writes in order to give a software development team overall guidance to the architecture of the software project</p>
+					<button class="upper">show project</button>
+				</div>
+			</div>
+
+		</div>
+		<button class="show-more upper">Show More</button>
+	</div>
+</div>
+<!-- End Oui Work -->
+
+<!-- Start Testimonials -->
+<div class="testim text-center">
+	<div class="overlay">
+		<div class="container">
+			<div class="client active ">
+				<img src="img/9.jpeg" alt="test">
+				<p>software designer writes in order to give a software development team overall <br> guidance to the architecture of the software project</p>
+				<span>Abdou Kaid</span>
+			</div>
+
+			<div class="client hidden">
+				<img src="img/8.jpg" alt="test">
+				<p>software designer writes in order to give a software development team overall <br> guidance to the architecture of the software project</p>
+				<span>Simo Kaid</span>
+			</div>
+
+			<div class="client hidden">
+				<img src="img/6.jpg" alt="test">
+				<p>software designer writes in order to give a software development team overall <br> guidance to the architecture of the software project</p>
+				<span>Amine Kaid</span>
+			</div>
+
+			<div class="client hidden">
+				<img src="img/7.jpg" alt="test">
+				<p>software designer writes in order to give a software development team overall <br> guidance to the architecture of the software project</p>
+				<span>Mehdi Kaid</span>
+			</div>
+
+			<i class="fa fa-chevron-right fa-3x"></i>
+			<i class="fa fa-chevron-left fa-3x"></i>
+		</div>
+	</div>
+</div>
+<!-- End Testimonials -->
+
+
+<!-- Start Our Team -->
+<div class="our-team text-center">
+	<div class="container">
+		<h1 class="upper">our <span class="main-color">Team</span></h1>
+		<p class="main">software designer writes in order to give a software development team <br> overall guidance to the architecture of the software project</p>
+
+		<div class="the-team">
+
+			<div class="person animation-element slide-left">
+				<img src="img/img-storie/41-400.jpg" alt="test">
+				<h3 class="upper">Abdou kaid</h3>
+				<div class="social">
+					<i class="fa fa-facebook-square fa-2x"></i>
+					<i class="fa fa-twitter-square fa-2x"></i>
+					<i class="fa fa-google-plus-square fa-2x"></i>
+				</div>
+				<p>software designer writes in order to give a software development team overall</p>
+			</div>
+
+			<div class="person animation-element slide-left">
+				<img src="img/img-storie/84-400.jpeg">
+				<h3 class="upper">Hanane kaid</h3>
+				<div class="social">
+					<i class="fa fa-facebook-square fa-2x"></i>
+					<i class="fa fa-twitter-square fa-2x"></i>
+					<i class="fa fa-google-plus-square fa-2x"></i>
+				</div>
+				<p>software designer writes in order to give a software development team overall</p>
+			</div>
+
+			<div class="person animation-element slide-left">
+				<img src="img/img-storie/11-400.jpeg" alt="test">
+				<h3 class="upper">Simo kaid</h3>
+				<div class="social">
+					<i class="fa fa-facebook-square fa-2x"></i>
+					<i class="fa fa-twitter-square fa-2x"></i>
+					<i class="fa fa-google-plus-square fa-2x"></i>
+				</div>
+				<p>software designer writes in order to give a software development team overall</p>
+			</div>
+
+			<div class="person animation-element slide-left">
+				<img src="img/9.jpeg" alt="test">
+				<h3 class="upper">Omar kaid</h3>
+				<div class="social">
+					<i class="fa fa-facebook-square fa-2x"></i>
+					<i class="fa fa-twitter-square fa-2x"></i>
+					<i class="fa fa-google-plus-square fa-2x"></i>
+				</div>
+				<p>software designer writes in order to give a software development team overall</p>
+			</div>
+
+		</div>
+	</div>
+</div>
+<!-- End Our Team -->
+
+
+<!-- Start Contact Form -->
+<div class="conact text-center">
+	<div class="overlay">
+		<div class="container">
+			<h1 class="upper">say <span class="main-color">Hello</span></h1>
+			<p class="main">software designer writes in order to give a software development team <br> overall guidance to the architecture of the software project</p>
+			<form>
+				<input type="email" name="mail" placeholder="Your Email">
+				<input type="text" name="subject" placeholder="Subject">
+				<textarea placeholder="Message"></textarea>
+
+				<div class="info">
+					<button class="upper">hire us</button>
+					<span class="social">
+						<i class="fa fa-facebook-square fa-2x"></i>
+						<i class="fa fa-twitter-square fa-2x"></i>
+						<i class="fa fa-google-plus-square fa-2x"></i>
+					</span>
+				</div>
+
+			</form>
+		</div>
+	</div>
+</div>
+<!-- End Contact Form -->
+
+<!-- Start Footer -->
+<div class="footer">
+	<div class="container">
+		<span class="copy upper">&copy; 2017 Copyright Design <span class="main-color"> Abdou Kaid</span></span>
+		<span class="design">abdessamadidel8@gmail.com</span>
+	</div>
+</div>
+<!-- End Footer -->
+
+<!-- JQuery -->
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/jquery.nicescroll.min.js"></script>
+<!-- My Js Plugins -->
+<script src="js/plugins.js"></script>
+</body>
+</html>
